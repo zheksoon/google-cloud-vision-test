5681f741f4ff581c0efd0590b1797a1f16a08bc2
Eugene Daragan <zheksoon@gmail.com>
2016-11-22 15:40:37 +0300

    port

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.static(path.join(__dirname, 'frontend', 'static')))
 
 // })
 
-app.listen(8000, () => {
-	console.log(`listening port 8000...`);
-})
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
